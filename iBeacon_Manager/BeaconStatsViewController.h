1f1a234b3d115298aa9f985089314a0b4c9108d1
David Crow <jdavidcrow@gmail.com>
2013-11-14 16:26:08 -0800

    added beacon stats view

---
+++ iBeacon_Manager/BeaconStatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BeaconStatsViewController.h
+//  iBeacon_Manager
+//
+//  Created by David Crow on 11/14/13.
+//  Copyright (c) 2013 David Crow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeaconStatsViewController : UITableViewController
+
+@end
---
+++ iBeacon_Manager/BeaconStatsViewController.m
@@ -0,0 +1,43 @@
+//
+//  BeaconStatsViewController.m
+//  iBeacon_Manager
+//
+//  Created by David Crow on 11/14/13.
+//  Copyright (c) 2013 David Crow. All rights reserved.
+//
+
+#import "BeaconStatsViewController.h"
+
+@interface BeaconStatsViewController ()
+
+@end
+
+@implementation BeaconStatsViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
