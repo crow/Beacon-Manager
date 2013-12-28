5784c5a5c64a8064e12aa3c07d7d90629fc2dbd0
David Crow <jdavidcrow@gmail.com>
2013-12-27 17:14:31 -0800

    removed constants c/p

--- iBeacon_Manager/BeaconManagerValues.h
+++ iBeacon_Manager/BeaconManagerValues.h
@@ -1,17 +1,14 @@
 //
-//  MotherShipValues.h
-//  MotherShip
+//  BeaconStatsViewController.h
+//  iBeacon_Manager
 //
-//  Created by Jeff Towle on 10/30/13.
-//  Copyright (c) 2013 Urban Airship. All rights reserved.
+//  Created by David Crow on 12/27/13.
+//  Copyright (c) 2013 David Crow. All rights reserved.
 //
 
-#ifndef MotherShip_MotherShipValues_h
-#define MotherShip_MotherShipValues_h
+//Define all the user defaults keys here
 
-#define kMotherShipSoundEnabled @"ua-mothership-apns-sound-enabled"
-#define kMotherShipModalKablamsEnabled @"ua-mothership-modal-kablams-enabled"
-#define kMotherShipiBeaconsEnabled @"ua-mothership-ibeacons-enable"
+#define kSomeConstat1 @"key0"
+#define kSomeConstat2 @"key1"
+#define kSomeConstat3 @"key2"
 
-
-#endif
